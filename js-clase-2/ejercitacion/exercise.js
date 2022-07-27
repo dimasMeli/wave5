c803fa5c4d0f7e8dbedd8490b5f4eae63e0fdc22
Dimas Mendoza <dimas.mendoza@mercadolibre.com.co>
2022-07-27 09:38:06 -0500

    end exercises of js

--- js-clase-2/ejercitacion/exercise.js
+++ js-clase-2/ejercitacion/exercise.js
@@ -5,22 +5,35 @@ PUNTO 1
 utiliza addingIdToProduct para agregarle un id único a cada producto empezando en 1
 *************************************************************************************/
 // Tu código acá
-let addingIdToProduct;
+
+const addingIdToProduct = () => {
+  const result = products.map((product, index) => ({
+    id: index + 1,
+    ...product,
+  }));
+  return result;
+};
 
 /*****************************************************************************
  * PUNTO 2
  * utiliza returningTheNames para retornar el nombre de cada uno de los productos
  ******************************************************************************/
 // Tu código acá
-let returningTheNames;
-
+const returningTheNames = () => {
+  const names = products.map((product) => product.name);
+  return names;
+};
 /********************************************************************************
 PUNTO 3
 utiliza searchID para retornar el producto cuyo id corresponda al parametro pasado.
 Si ejecuto searchID(3) debería devolver el objeto entero, cuyo id sea 3
 ************************************************************************************/
 // Tu código acá
-let searchID;
+const searchID = (id) => {
+  const findProduct = addingIdToProduct().find((product) => product.id === id);
+  const existProduct = findProduct ? findProduct : "no hubo coincidencias";
+  return existProduct;
+};
 
 /*****************************************************************************
 PUNTO 4
@@ -28,13 +41,19 @@ utiliza matchingColors para retornar los productos que hagan match con
 el color pasado por parámetro
 ******************************************************************************/
 // Tu código acá
-let matchingColors;
+const matchingColors = (color) => {
+  const selectColors = addingIdToProduct().filter((product) =>
+    product.colors.includes(color)
+  );
+  return selectColors;
+};
 /*****************************************************************************
 PUNTO 5
 utiliza colorsLength para retornar los productos que no tengan color
 ******************************************************************************/
 // Tu código acá
-let colorsLength;
+const colorsLength = () =>
+  addingIdToProduct().filter((product) => product.colors.length === 0);
 
 /*****************************************************************************
 PUNTO 6
@@ -43,21 +62,32 @@ propiedades (name,price,quantity,colors).
 Retornar los productos donde se incluya el producto agregado
 ******************************************************************************/
 // Tu código acá
-let addProduct;
+const addProduct = (newProduct) => {
+  const { name, price, quantity, colors } = newProduct;
+  const addingProduct =
+    name && price && quantity && colors
+      ? products.push(newProduct)
+      : "no se pudo agregar";
+  return addingProduct;
+};
 
 /*****************************************************************************
 PUNTO 7
 utiliza deleteProduct para Eliminar del array de productos a aquellos sin stock (con quantity 0)
 ******************************************************************************/
 // Tu código acá
-let deleteProduct;
+const deleteProduct = () => {
+  const deleteProducts = products.filter((product) => product.quantity > 0);
+  return deleteProducts;
+};
 /*****************************************************************************
 PUNTO 8
 utiliza existingProducts para sumar el numero total de stock entre todos los productos.
 Debe retornar dicho numero
 ******************************************************************************/
 // Tu código acá
-let existingProducts;
+const existingProducts = (products) =>
+  products.reduce((total, product) => total + product.quantity, 0);
 
 /*****************************************************************************
 PUNTO 9
@@ -67,16 +97,17 @@ showHigherPrice(products,500)
 debería devolver 2 objetos, ya que solo 2 productos tienen un valor mayor a 500
 ******************************************************************************/
 // Tu código acá
-let showHigherPrice;
+const showHigherPrice = (products, price) =>
+  products.filter((product) => product.price > price);
 
 module.exports = {
-	addingIdToProduct,
-	existingProducts,
-	deleteProduct,
-	addProduct,
-	colorsLength,
-	matchingColors,
-	searchID,
-	returningTheNames,
-	showHigherPrice,
+  addingIdToProduct,
+  existingProducts,
+  deleteProduct,
+  addProduct,
+  colorsLength,
+  matchingColors,
+  searchID,
+  returningTheNames,
+  showHigherPrice,
 };
